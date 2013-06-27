ad712fc1d1499d5f23322bde32df4ab28a6f80ab
Olivier Amblet <olivier.amblet@webdoc.com>
2013-06-27 20:55:36 +0200

    click, samples, specs

---
+++ spec/mouse-events-spec.js
@@ -0,0 +1,46 @@
+var expect = chai.expect;
+
+describe("trigger MouseEvents", function() {
+  describe("click", function() {
+    var button;
+    beforeEach(function() {
+      button = document.createElement('button');
+      document.body.appendChild(button);
+    });
+    afterEach(function() {
+      document.body.removeChild(button);
+    });
+
+    it('initiates a click with the default value', function(done) {
+      button.addEventListener('click', function(event) {
+        expect(event.type).to.eql('click');
+        expect(event.bubbles).to.eql(true);
+        expect(event.cancelable).to.eql(true);
+        done();
+      }, false);
+      trigger(button, 'click');
+    });
+
+    it('initiates click with custom options', function(done) {
+      button.addEventListener('click', function(event) {
+        expect(event.bubbles).to.eql(false);
+        expect(event.cancelable).to.eql(false);
+        expect(event.screenX).to.eql(1);
+        expect(event.screenY).to.eql(2);
+        expect(event.clientX).to.eql(3);
+        expect(event.clientY).to.eql(4);
+        expect(event.ctrlKey).to.eql(true);
+        done();
+      }, false);
+      trigger(button, 'click', {
+        canBubble: false,
+        cancelable: false,
+        ctrlKey: true,
+        screenX: 1,
+        screenY: 2,
+        clientX: 3,
+        clientY: 4
+      });
+    });
+  });
+});
--- trigger-dom-event.js
+++ trigger-dom-event.js
@@ -1,12 +1,83 @@
 (function(module, document){
   "use strict";
 
+  function merge () {
+    var target = {};
+    for(var i = 0; i < arguments.length; i++) {
+      var source = arguments[i];
+      if (source) {
+        for (var name in source) {
+          target[name] = source[name];
+        }
+      }
+    }
+    return target;
+  }
+
+  var properties = {
+    initMouseEvent: {
+      canBubble: true,
+      cancelable: true,
+      view: window,
+      detail: 1,
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      button: 0,
+      relatedTarget: null
+    }
+  };
+
   // Describe Events default properties.
+  // Documentation coming from MDN.
   var events = {
+    // Mouse Events:
+    // click, mousedown, mouseup, mouseover, mousemove, mouseout
     click: {
-      type: 'MouseEvent',
-      bubbles: true,
-      cancelable: true
+      type: "MouseEvents"
+    }
+  };
+
+  var initializers = {
+    MouseEvents: function (event, name, options) {
+      var p = merge(properties.initMouseEvent, options);
+      return event.initMouseEvent(
+        name,
+        // Properties you can override in initMouseEvent:
+        // canBubble: whether or not the event can bubble. Sets the value of event.bubbles.
+        p.canBubble,
+        // cancelable: whether or not the event's default action can be prevented. Sets the value of event.cancelable.
+        p.cancelable,
+        // view: the Event's AbstractView. You should pass the window object here. Sets the value of event.view.
+        p.view,
+        // detail: the Event's mouse click count. Sets the value of event.detail.
+        p.detail,
+        // screenX: the Event's screen x coordinate. Sets the value of event.screenX.
+        p.screenX,
+        // screenY: the Event's screen y coordinate. Sets the value of event.screenY.
+        p.screenY,
+        // clientX: the Event's client x coordinate. Sets the value of event.clientX.
+        p.clientX,
+        // clientY: the Event's client y coordinate. Sets the value of event.clientY.
+        p.clientY,
+        // ctrlKey: whether or not control key was depressed during the Event. Sets the value of event.ctrlKey.
+        p.ctrlKey,
+        // altKey: whether or not alt key was depressed during the Event. Sets the value of event.altKey.
+        p.altKey,
+        // shiftKey: whether or not shift key was depressed during the Event. Sets the value of event.shiftKey.
+        p.shiftKey,
+        // metaKey: whether or not meta key was depressed during the Event. Sets the value of event.metaKey.
+        p.metaKey,
+        // button: the Event's mouse event.button.
+        p.button,
+        // relatedTarget: the Event's related EventTarget. Only used with some event types (e.g. mouseover and mouseout). In other cases, pass null.
+        p.relatedTarget
+      );
     }
   };
 
@@ -18,10 +89,9 @@
     if (!events[name]) {
       throw new Error('Unknown Event: ' + name);
     }
-    properties = events[name];
     if (document.createEvent) {
-      event = document.createEvent("MouseEvents");
-      event.initEvent(name, properties.bubbles, properties.cancelable);
+      event = document.createEvent(events[name].type);
+      initializers[events[name].type](event, name, options);
     } else {
       event = document.createEventObject();
       event.eventType = name;
@@ -34,7 +104,6 @@
    */
   function triggerEvent (element, event) {
     if (document.createEvent) {
-      console.log('will dispatch', event, 'on', element);
       element.dispatchEvent(event);
     } else {
       element.fireEvent("on" + event.eventType, event);
